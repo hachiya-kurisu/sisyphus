82403b67eeaaaa35842aaee377c383f73821c3c4
蜂谷栗栖 <kurisu@blekksprut.net>
2024-12-15 17:10:47 +0900

    -v to show version

--- cmd/sisyphus/main.go
+++ cmd/sisyphus/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"blekksprut.net/sisyphus"
 	"flag"
+	"fmt"
 	"os"
 )
 
 func main() {
+	v := flag.Bool("v", false, "version")
 	f := flag.String("f", "html", "choose flavor (html/markdown)")
 	flag.Parse()
+
+	if *v {
+		fmt.Printf("%s %s\n", os.Args[0], sisyphus.Version)
+		os.Exit(0)
+	}
 
 	var flavor sisyphus.Flavor
 	switch *f {
