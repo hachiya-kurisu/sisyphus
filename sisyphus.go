862d9ee4b9f84304cbd9ca35f2a69787eaf7c17c
蜂谷栗栖 <kurisu@blekksprut.net>
2025-01-02 09:36:55 +0900

    bump version

--- sisyphus.go
+++ sisyphus.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-const Version = "0.3.6"
+const Version = "0.3.7"
 
 type LinkHook func(uri string, text string, match string) string
 type QuoteHook func(text string) string
